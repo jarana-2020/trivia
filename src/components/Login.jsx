3335483ab05400d54b7480fbcd8692bc45ab7763
Julio <julioaranabr@yahoo.com.br>
2021-12-13 17:14:32 -0300

    Altered Form login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,37 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import SendIcon from '@mui/icons-material/Send';
+import Box from '@mui/material/Box';
+import '../App.css';
 
 const Login = () => (
-  <section>
-    <label htmlFor="name">
-      Player Name:
-      <input
-        type="text"
-        id="name"
-      />
-    </label>
-    <label htmlFor="email">
-      E-mail:
-      <input
-        type="email"
-        id="email"
-      />
-    </label>
-    <input type="button" value="Play" />
-  </section>
+  <Box
+    component="section"
+    className="section-login"
+    sx={ { maxWidth: 'sm' } }
+  >
+    <TextField
+      id="outlined-name"
+      label="Player Name"
+      variant="standard"
+      fullWidth
+    />
+    <TextField
+      type="email"
+      id="outlined-email"
+      label="E-mail"
+      variant="standard"
+      margin="normal"
+      fullWidth
+    />
+    <Button
+      variant="contained"
+      endIcon={ <SendIcon /> }
+    >
+      Play
+    </Button>
+  </Box>
 );
 
 export default Login;
