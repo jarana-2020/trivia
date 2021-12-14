4042bdd597e9b9f5f6be2704c7906a9a92154bda
Julio <julioaranabr@yahoo.com.br>
2021-12-14 14:35:56 -0300

    Installed Redux Toolkit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-import Game from './pages/Game';
-import Login from './pages/Login';
+import Game from './pages/game';
+import Login from './pages/login';
 
 export default function App() {
   return (
--- src/tests/pages/Login.spec.tsx
+++ src/tests/pages/Login.spec.tsx
@@ -1,4 +1,4 @@
-import Login from "../../pages/Login";
+import Login from "../../pages/login";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
