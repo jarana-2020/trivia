9599eda7de4608e4fd7c62ee54fd52eb0b3f4344
Julio <julioaranabr@yahoo.com.br>
2021-12-14 14:02:43 -0300

    Implemented Tests Login Page

---
+++ src/tests/pages/Login.spec.tsx
@@ -0,0 +1,31 @@
+import Login from "../../components/Login";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+describe("Testing Login Page", () => {
+  it("should have a field for player name" , () => {
+    render(<Login />);
+
+    const inputName = screen.getByLabelText('Player Name');
+    userEvent.type(inputName, 'Julio');
+    expect(inputName).toHaveValue('Julio');
+    expect(inputName).toBeInTheDocument();
+  })
+
+  it("should have a field for player e-mail" , () => {
+    render(<Login />);
+
+    const inputEmail = screen.getByLabelText('E-mail');
+    userEvent.type(inputEmail, 'julio@yahoo.com.br');
+    expect(inputEmail).toHaveValue('julio@yahoo.com.br');
+    expect(inputEmail).toBeInTheDocument();
+  })
+
+  it("should have a button for start the trivia game" , () => {
+    render(<Login />);
+
+    const btnPlay = screen.getByRole('button', { name: /Play/i});
+    expect(btnPlay).toBeInTheDocument();
+    expect(btnPlay).toBeDisabled();
+  })
+})
