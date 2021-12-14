97ac98c37bb2746dc963d0ea9a8778e90d11db51
Julio <julioaranabr@yahoo.com.br>
2021-12-13 22:58:35 -0300

    Implemented Login Layout

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import SendIcon from '@mui/icons-material/Send';
+import InputAdornment from '@mui/material/InputAdornment';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MailIcon from '@mui/icons-material/Mail';
 import Box from '@mui/material/Box';
+import logo from '../images/trivia.png';
 import '../App.css';
+
+const validateEmail = (email) => {
+  const regEX = /\S+@\S+\.\S+/;
+  return regEX.test(email);
+};
+
+const getDataInfo = (email, name) => {
+  if (validateEmail(email) && name.length > 0) {
+    return false;
+  }
+  return true;
+};
+
+const renderLoginEntries = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const verifyValues = getDataInfo(email, name);
+  return (
+    <>
+      <TextField
+        autoComplete="off"
+        id="outlined-name"
+        InputProps={ {
+          startAdornment: (
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          ),
+        } }
+        label="Player Name"
+        variant="standard"
+        value={ name }
+        fullWidth
+        onChange={ ({ target }) => setName(target.value) }
+      />
+      <TextField
+        autoComplete="off"
+        type="email"
+        id="outlined-email"
+        InputProps={ {
+          startAdornment: (
+            <InputAdornment position="start">
+              <MailIcon />
+            </InputAdornment>
+          ),
+        } }
+        label="E-mail"
+        variant="standard"
+        value={ email }
+        margin="normal"
+        fullWidth
+        onChange={ ({ target }) => setEmail(target.value) }
+      />
+      <Button
+        variant="contained"
+        endIcon={ <SendIcon /> }
+        disabled={ verifyValues }
+      >
+        Play
+      </Button>
+    </>
+  );
+};
 
 const Login = () => (
   <Box
@@ -11,26 +78,12 @@ const Login = () => (
     className="section-login"
     sx={ { maxWidth: 'sm' } }
   >
-    <TextField
-      id="outlined-name"
-      label="Player Name"
-      variant="standard"
-      fullWidth
+    <img
+      alt="logo-trivia"
+      className="img-logo"
+      src={ logo }
     />
-    <TextField
-      type="email"
-      id="outlined-email"
-      label="E-mail"
-      variant="standard"
-      margin="normal"
-      fullWidth
-    />
-    <Button
-      variant="contained"
-      endIcon={ <SendIcon /> }
-    >
-      Play
-    </Button>
+    {renderLoginEntries()}
   </Box>
 );
 
