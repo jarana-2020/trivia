6d8053e578c2666d6948b5d04514212891db1882
Julio <julioaranabr@yahoo.com.br>
2021-12-14 22:56:11 -0300

    Altered routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Game from './pages/game';
 import Login from './pages/login';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Route exact path="/" component={ Login } />
+    <Switch>
       <Route path="/game" component={ Game } />
-    </BrowserRouter>
+      <Route exact path="/" component={ Login } />
+    </Switch>
   );
 }
