7999cc0539fa966030b5cbf2dee9b81845499078
Julio <julioaranabr@yahoo.com.br>
2021-12-16 14:47:52 -0300

    Add test in game

--- src/tests/Game.spec.tsx
+++ src/tests/Game.spec.tsx
@@ -4,10 +4,15 @@ import Game from '../pages/Game';
 
 
 describe("Testing Game Page", () => {
-  render(<Game />)
-
   it("should have a button configuration", () => {
+    render(<Game />)
     const btnConfiguration = screen.getByLabelText('configuration');
     expect(btnConfiguration).toBeInTheDocument();
+  })
+
+  it("should have a image", () => {
+    render(<Game />)
+    const imgLogo = screen.getByAltText(/logo-trivia/);
+    expect(imgLogo).toBeInTheDocument();
   })
 })
