7fd20fba1fc80ee29eb3fe90e1e879c9c0a3ac3b
Julio <julioaranabr@yahoo.com.br>
2021-12-21 19:01:53 -0300

    created component loading

---
+++ src/components/Loading.jsx
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+const Loading = () => {
+  <Typography
+    data-testid="question-category"
+    variant="h3"
+    component="p"
+  >
+    Loading...Loading...Loading
+  </Typography>;
+};
+
+export default Loading;
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -2,17 +2,22 @@ import { Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
 import { getQuestions, selectQuestions } from '../features/game/gameSlice';
 
 const Game = () => {
   const dispatch = useDispatch();
   const questions = useSelector(selectQuestions);
   const [questionIndex] = useState(0);
-  console.log(questions.length, questionIndex);
+  const haveData = questions.length > 0;
 
   useEffect(() => {
     dispatch(getQuestions());
   }, []);
+
+  if (!haveData) {
+    return <Loading />;
+  }
 
   return (
     <>
@@ -22,7 +27,7 @@ const Game = () => {
         variant="h6"
         component="p"
       >
-        {/* {category} */}
+        teste
 
       </Typography>
       <Typography
@@ -30,7 +35,7 @@ const Game = () => {
         variant="h6"
         component="p"
       >
-        {/* {question} */}
+        teste
 
       </Typography>
     </>
