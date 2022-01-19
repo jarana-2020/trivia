e7e7b87c4c37cfe342d717887d7a1decc5945946
Julio <julioaranabr@yahoo.com.br>
2022-01-19 10:30:26 -0300

    created message feedback

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,20 +1,45 @@
+import { Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Header from '../components/Header';
+import { selectAssertions } from '../features/player/playerSlice';
 
-const Feedback = () => (
-  <>
-    <Header />
-    <Box
-      sx={ {
-        mt: '20px',
-        border: '1px black solid',
-        justifyContent: 'center',
-        display: 'flex',
-        width: '100%' } }
-    />
-  </>
+const getFeedbackMessage = (assertions) => {
+  const MIN_RESULT = 3;
+  if (assertions < MIN_RESULT) {
+    return '"Could be better..."';
+  }
+  if (assertions >= MIN_RESULT) {
+    return '"Nice job!"';
+  }
+};
 
-);
+const Feedback = () => {
+  const assertions = useSelector(selectAssertions);
+  return (
+    <>
+      <Header />
+      <Box
+        sx={ {
+          mt: '20px',
+          border: '1px black solid',
+          justifyContent: 'center',
+          display: 'flex',
+          width: '100%' } }
+      >
+        <Typography
+          data-testid="player-name"
+          variant="h6"
+          component="p"
+          style={ { marginRight: '5px' } }
+        >
+          {getFeedbackMessage(assertions)}
+
+        </Typography>
+      </Box>
+    </>
+  );
+};
 
 export default Feedback;
