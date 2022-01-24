0e90805ec235c12fb0767c7769017c81bf0489e5
Julio <julioaranabr@yahoo.com.br>
2022-01-24 10:52:29 -0300

    created tests for ranking page

---
+++ src/tests/Ranking.spec.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { Provider } from 'react-redux';
+import Ranking from '../pages/Ranking';
+import store from '../store';
+
+
+describe("Testing Ranking Page", () => {
+
+  it("should have a title", () => {
+    render(
+      <Provider store={store}>
+        <Ranking />
+      </Provider>
+    );
+    const title = screen.getByTestId(/^ranking-title$/);
+    expect(title).toBeInTheDocument();
+  })
+
+  it("should have player name", () => {
+    render(
+      <Provider store={store}>
+        <Ranking />
+      </Provider>
+    );
+    const playerName = screen.getByTestId(/^player-name-0$/);
+    expect(playerName).toBeInTheDocument();
+  })
+
+  it("should have player score", () => {
+    render(
+      <Provider store={store}>
+        <Ranking />
+      </Provider>
+    );
+    const playerScore = screen.getByTestId(/^player-score-0$/);
+    expect(playerScore).toBeInTheDocument();
+  })
+
+});
