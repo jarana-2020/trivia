95667466e9fc0ed9a782b8909b81ff87ca0d600d
Julio <julioaranabr@yahoo.com.br>
2022-02-07 12:13:23 -0300

    saved ranking in local storage

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import FeedbackButtons from '../components/FeedbackButtons';
 import FeedbackMessage from '../components/FeedbackMessage';
 import Header from '../components/Header';
-import { selectAssertions, selectScore } from '../features/player/playerSlice';
+import { selectAssertions, selectEmail,
+  selectName, selectScore } from '../features/player/playerSlice';
+import getGravatar from '../services/requestGravatar';
 
 const getFeedbackMessage = (assertions) => {
   const MIN_RESULT = 3;
@@ -24,6 +26,29 @@ const Feedback = () => {
   const assertions = useSelector(selectAssertions);
   const score = useSelector(selectScore);
   const history = useHistory();
+  const playerName = useSelector(selectName);
+  const playerEmail = useSelector(selectEmail);
+
+  const saveRankingStorage = () => {
+    const playerRanking = [
+      { name: playerName, score, picture: getGravatar(playerEmail) }];
+
+    if (localStorage.getItem('ranking')) {
+      const { name, gravatarEmail, score: playerScore } = JSON
+        .parse(localStorage.getItem('player'));
+
+      const ranking = JSON.parse(localStorage.getItem('ranking'));
+      ranking.push({ name, score: playerScore, picture: getGravatar(gravatarEmail) });
+      localStorage.setItem('ranking', JSON.stringify(ranking));
+      return;
+    }
+    localStorage.setItem('ranking', JSON.stringify(playerRanking));
+  };
+
+  useEffect(() => {
+    saveRankingStorage();
+  }, []);
+
   return (
     <>
       <Header />
