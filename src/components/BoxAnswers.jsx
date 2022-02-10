4db2090a5d0ecdc5e48f84226c33607d688c58c9
Julio <julioaranabr@yahoo.com.br>
2022-02-10 11:37:54 -0300

    refatored box answers

--- src/components/BoxAnswers.jsx
+++ src/components/BoxAnswers.jsx
@@ -1,36 +1,39 @@
-import { Button } from '@mui/material';
-import { Box } from '@mui/system';
 import Proptypes from 'prop-types';
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { alterScore } from '../features/player/playerSlice';
-import { calcScore } from '../helper/helper';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { alterScore, selectAssertions,
+  selectScore } from '../features/player/playerSlice';
+import { b64ToUtf8, calcScore } from '../helper/helper';
 import ButtonNext from './ButtonNext';
-
-const alterBorderColor = (answer, correctAnswer) => {
-  if (answer === correctAnswer) {
-    return '3px solid rgb(6, 240, 15)';
-  }
-  return '3px solid rgb(255, 0, 0)';
-};
+import ButtonsQuestions from './ButtonsQuestions';
 
 const getScore = async (timerAndScore, level, valueQuestion, dispatch) => {
-  const { time, playerScore } = timerAndScore;
+  const { time, score } = timerAndScore;
   if (valueQuestion === 'correct') {
-    dispatch(alterScore(calcScore(time, level) + playerScore));
+    dispatch(alterScore(calcScore(time, b64ToUtf8(level)) + score));
   }
+};
+
+const saveScoreStorage = (assertions, score) => {
+  const dataStorage = JSON.parse(localStorage.getItem('player'));
+  const playerInfo = { ...dataStorage, assertions, score };
+  localStorage.setItem('player', JSON.stringify(playerInfo));
 };
 
 const BoxAnswers = ({ arrayQuestions, time, questionIndex,
-  stopTimer, handleClick, isAnswered, setIsAnswered, playerScore }) => {
+  stopTimer, handleClick, isAnswered, setIsAnswered }) => {
   const { shuffledQuestions } = arrayQuestions[questionIndex];
   const correctAnswer = arrayQuestions[questionIndex].correct_answer;
   const level = arrayQuestions[questionIndex].difficulty;
+  const score = useSelector(selectScore);
+  const assertions = useSelector(selectAssertions);
   const dispatch = useDispatch();
+
+  useEffect(() => saveScoreStorage(assertions, score), [score]);
 
   const timeAndScore = {
     time,
-    playerScore,
+    score,
   };
 
   const handleClickAnswer = ({ target }) => {
@@ -43,26 +46,15 @@ const BoxAnswers = ({ arrayQuestions, time, questionIndex,
   return (
     <>
       {shuffledQuestions.map((answer, index) => (
-        <Box
+        <ButtonsQuestions
           key={ index }
-          className="box-answers"
-        >
-          <Button
-            data-testid={ answer === correctAnswer
-              ? 'correct-answer'
-              : 'wrong-answer' }
-            value={ answer === correctAnswer
-              ? 'correct'
-              : 'wrong' }
-            onClick={ handleClickAnswer }
-            disabled={ time === 0 || isAnswered }
-            sx={ {
-              border: isAnswered ? alterBorderColor(answer, correctAnswer) : null,
-            } }
-          >
-            {answer}
-          </Button>
-        </Box>
+          answer={ answer }
+          correctAnswer={ correctAnswer }
+          handleClickAnswer={ handleClickAnswer }
+          isAnswered={ isAnswered }
+          time={ time }
+        />
+
       ))}
       <ButtonNext
         isAnswered={ isAnswered }
@@ -80,7 +72,6 @@ BoxAnswers.propTypes = {
   handleClick: Proptypes.func.isRequired,
   setIsAnswered: Proptypes.func.isRequired,
   isAnswered: Proptypes.bool.isRequired,
-  playerScore: Proptypes.number.isRequired,
 };
 
 export default BoxAnswers;
